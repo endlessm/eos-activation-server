5562a83ff54086442eb5733b4eaeb59b72f6b56c
Srdjan Grubor <sgnn7@sgnn7.org>
2016-07-12 10:00:30 -0500

    Moved to logging through winston
    console.log is synchronous so we use the async winston logging library.

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 'use strict';
 
 var Express = require('express');
+var winston = require('winston');
 
 var app = Express();
 
+var LOGGING_LEVEL = 'debug';
 var HTTP_PORT = 8080;
 
 app.get('/activate', function (req, res) {
   res.send('OK');
 });
 
-console.log('Server starting on port ' + HTTP_PORT + '...');
+winston.info('Server starting on port ' + HTTP_PORT + '...');
 
 app.listen(HTTP_PORT, function () {
-  console.log('Server started');
+  winston.info('Server started');
 });
