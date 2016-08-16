a6e106a9744b060d797e72cdfc7d2332c106ce36
Srdjan Grubor <sgnn7@sgnn7.org>
2016-08-16 16:54:05 -0500

    Added ping model - liable to change though
    This will allow us to write the basic API for /ping

---
+++ models/ping.model.js
@@ -0,0 +1,40 @@
+// vim: ts=2 sw=2 expandtab
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('Ping', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    image: DataTypes.STRING,
+    vendor: DataTypes.STRING,
+    product: DataTypes.STRING,
+    release: DataTypes.STRING,
+    count: DataTypes.BIGINT,
+    country: DataTypes.STRING,
+    region: DataTypes.STRING,
+    city: DataTypes.STRING,
+    latitude: DataTypes.FLOAT,
+    longitude: DataTypes.FLOAT
+  }, {
+    timestamps: true,
+    paranoid: false,
+    freezeTableName: true,
+    tableName: 'activation',
+    indexes: [
+      { fields: ['image']     },
+      { fields: ['vendor']    },
+      { fields: ['product']   },
+      { fields: ['release']   },
+      { fields: ['count']     },
+      { fields: ['country']   },
+      { fields: ['region']    },
+      { fields: ['city']      },
+      { fields: ['latitude']  },
+      { fields: ['longitude'] }
+    ]
+  });
+};
